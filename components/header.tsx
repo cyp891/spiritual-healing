181590f6659d7cca16fb086d3fc76d420655f3b8
cyp891 <cyp892@yahoo.com>
2025-11-26 19:36:24 +0200

    mobile

--- components/header.tsx
+++ components/header.tsx
@@ -60,6 +60,7 @@ export default function Header() {
             {/* <button className="w-full bg-primary text-primary-foreground px-6 py-2 rounded-lg hover:bg-primary/90 transition-colors">
               Get Started
             </button> */}
+            <LanguageSwitcher />
           </div>
         </div>
       )}
